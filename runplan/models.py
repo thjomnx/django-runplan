d1ddc87f306ea736d781783f088296e04ddb302d
Markus Enax <Markus.Enax@gmail.com>
2013-03-29 10:47:35 +0100

    Code is now Python2/Django 1.4.x compatible

--- runplan/models.py
+++ runplan/models.py
@@ -1,11 +1,15 @@
+from __future__ import unicode_literals
+
 import datetime
 
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 meettime_threshold=datetime.timedelta(minutes=15)
 
+@python_2_unicode_compatible
 class Run(models.Model):
     author = models.ForeignKey(User)
     create_date = models.DateTimeField('creation date', auto_now_add=True)
@@ -23,14 +27,15 @@ class Run(models.Model):
     def is_past(self):
         return self.meeting_date < timezone.now() + meettime_threshold
     
-    def __unicode__(self):
+    def __str__(self):
         return str(self.meeting_date.strftime('%d.%m.%Y %H:%M'))
 
+@python_2_unicode_compatible
 class Comment(models.Model):
     run = models.ForeignKey(Run)
     author = models.ForeignKey(User)
     create_date = models.DateTimeField('creation date', auto_now_add=True)
     comment_text = models.TextField(blank=True)
     
-    def __unicode__(self):
+    def __str__(self):
         return self.author.username
